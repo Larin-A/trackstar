c2e9f8f03bfc14c5f4a38246d85b2bd104b6d164
Александр Ларин <alex175alex@ro.ru>
2024-05-03 02:34:38 +0300

    project CRUD (глава 4)

---
+++ protected/migrations/m240502_233126_create_issue_user_and_assignment_tables.php
@@ -0,0 +1,25 @@
+<?php
+
+class m240502_233126_create_issue_user_and_assignment_tables extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m240502_233126_create_issue_user_and_assignment_tables does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
