74adbbeb28326530d721688af1549623aa1ece03
Александр Ларин <alex175alex@ro.ru>
2024-05-05 22:55:24 +0300

    Создание ролей

--- protected/controllers/ProjectController.php
+++ protected/controllers/ProjectController.php
@@ -20,36 +20,6 @@ class ProjectController extends Controller
 	}
 
 	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array(
-				'allow',  // allow all users to perform 'index' and 'view' actions
-				'actions' => array('index', 'view'),
-				'users' => array('*'),
-			),
-			array(
-				'allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions' => array('create', 'update'),
-				'users' => array('@'),
-			),
-			array(
-				'allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions' => array('admin', 'delete'),
-				'users' => array('admin'),
-			),
-			array(
-				'deny',  // deny all users
-				'users' => array('*'),
-			),
-		);
-	}
-
-	/**
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
 	 */
@@ -67,10 +37,12 @@ class ProjectController extends Controller
 				),
 			)
 		);
-		$this->render('view', array(
-			'model' => $this->loadModel($id),
-			'issueDataProvider' => $issueDataProvider,
-		)
+		$this->render(
+			'view',
+			array(
+				'model' => $this->loadModel($id),
+				'issueDataProvider' => $issueDataProvider,
+			)
 		);
 	}
 
@@ -91,9 +63,11 @@ class ProjectController extends Controller
 				$this->redirect(array('view', 'id' => $model->id));
 		}
 
-		$this->render('create', array(
-			'model' => $model,
-		)
+		$this->render(
+			'create',
+			array(
+				'model' => $model,
+			)
 		);
 	}
 
@@ -115,9 +89,11 @@ class ProjectController extends Controller
 				$this->redirect(array('view', 'id' => $model->id));
 		}
 
-		$this->render('update', array(
-			'model' => $model,
-		)
+		$this->render(
+			'update',
+			array(
+				'model' => $model,
+			)
 		);
 	}
 
@@ -141,9 +117,11 @@ class ProjectController extends Controller
 	public function actionIndex()
 	{
 		$dataProvider = new CActiveDataProvider('Project');
-		$this->render('index', array(
-			'dataProvider' => $dataProvider,
-		)
+		$this->render(
+			'index',
+			array(
+				'dataProvider' => $dataProvider,
+			)
 		);
 	}
 
@@ -157,9 +135,11 @@ class ProjectController extends Controller
 		if (isset($_GET['Project']))
 			$model->attributes = $_GET['Project'];
 
-		$this->render('admin', array(
-			'model' => $model,
-		)
+		$this->render(
+			'admin',
+			array(
+				'model' => $model,
+			)
 		);
 	}
 
