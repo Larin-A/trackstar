90896a56bda10a02de7011b68244329492d34c3d
Александр Ларин <alex175alex@ro.ru>
2024-05-05 21:03:49 +0300

    lastLogin (время) и аутентификация

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,10 +1,17 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
 ?>
 
+
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<?php if (!Yii::app()->user->isGuest): ?>
+	<p>
+		You last logged in on <?php echo Yii::app()->user->lastLogin; ?>.
+	</p>
+<?php endif; ?>
+
 
 <p>Congratulations! You have successfully created your Yii application.</p>
 
@@ -15,6 +22,6 @@ $this->pageTitle=Yii::app()->name;
 </ul>
 
 <p>For more details on how to further develop this application, please read
-the <a href="https://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="https://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+	the <a href="https://www.yiiframework.com/doc/">documentation</a>.
+	Feel free to ask in the <a href="https://www.yiiframework.com/forum/">forum</a>,
+	should you have any questions.</p>
