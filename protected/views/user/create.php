a50f1a108407dd82afa26c12f510982824e6a4d6
Александр Ларин <alex175alex@ro.ru>
2024-05-24 21:55:11 +0300

    Изменение формы регистрации

--- protected/views/user/create.php
+++ protected/views/user/create.php
@@ -2,17 +2,12 @@
 /* @var $this UserController */
 /* @var $model User */
 
-$this->breadcrumbs=array(
-	'Users'=>array('index'),
-	'Create',
+$this->breadcrumbs = array(
+	'Registration',
 );
 
-$this->menu=array(
-	array('label'=>'List User', 'url'=>array('index')),
-	array('label'=>'Manage User', 'url'=>array('admin')),
-);
 ?>
 
-<h1>Create User</h1>
+<h1>Registration</h1>
 
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php $this->renderPartial('_form', array('model' => $model)); ?>
